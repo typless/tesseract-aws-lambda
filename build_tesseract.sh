6496debdfd7db8b30ffe5fb75751d80e4672c8fc
AnastaciusWright <alexandronadal@gmail.com>
2022-04-17 14:04:12 +0200

    added libraries, updated library libtesseract from 4 to 5, and updated tesseract version to 5

--- build_tesseract.sh
+++ build_tesseract.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
-TESSERACT_VERSION="4.1.1"
+TESSERACT_VERSION="5.1.0"
 
 # docker_build tesseract
 cd ~
@@ -18,15 +18,19 @@ mkdir tesseract-standalone
 cd tesseract-standalone
 cp /usr/local/bin/tesseract .
 mkdir lib
-cp /usr/local/lib/libtesseract.so.4 lib/
+cp /usr/local/lib/libtesseract.so.5 lib/
 cp /usr/local/lib/liblept.so.5 lib/
 cp /usr/lib64/libjpeg.so.62 lib/
 cp /usr/lib64/libwebp.so.4 lib/
 cp /usr/lib64/libpng15.so.15 lib/
+cp /usr/lib64/libtiff.so.5 lib/
+cp /usr/lib64/libgomp.so.1 lib/
+cp /usr/lib64/libjbig.so.2.0 lib/
+
 mkdir tessdata
 cd tessdata
 
-wget https://github.com/tesseract-ocr/tessdata_fast/raw/master/eng.traineddata
+wget https://github.com/tesseract-ocr/tessdata_best/raw/main/eng.traineddata
 
 mkdir configs
 cp /usr/local/share/tessdata/configs/pdf configs/
