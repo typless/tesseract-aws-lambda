db7e841337b3ba776478cb125391a045300a5f4b
janbaticalgoritmik <jan.batic@algoritmik.si>
2023-07-30 23:07:29 +0200

    Update libraries, tesseract, python version.

--- Dockerfile
+++ Dockerfile
@@ -1,18 +1,13 @@
-FROM amazonlinux:2018.03.0.20200318.1
+FROM public.ecr.aws/lambda/python:3.10-x86_64
 
-ENV LEPTONICA_VERSION="1.75.1"
-# docker_build leptonica
+ENV LEPTONICA_VERSION="1.83.0"
+ENV TESSERACT_VERSION="5.3.0"
 WORKDIR /tmp/
-RUN yum install clang wget zip gzip tar autoconf xz libpng-devel libtiff-devel zlib-devel libwebp-devel libjpeg-turbo-devel make libtool pkgconfig -y
-RUN wget https://github.com/DanBloomberg/leptonica/releases/download/$LEPTONICA_VERSION/leptonica-$LEPTONICA_VERSION.tar.gz
-RUN tar -xzvf leptonica-$LEPTONICA_VERSION.tar.gz
-RUN cd leptonica-$LEPTONICA_VERSION && ./configure && make && make install
 
-RUN wget http://ftpmirror.gnu.org/autoconf-archive/autoconf-archive-2019.01.06.tar.xz
-RUN tar -xvf autoconf-archive-2019.01.06.tar.xz
-RUN cd autoconf-archive-2019.01.06 && ./configure && make && make install
-RUN cd autoconf-archive-2019.01.06 && cp m4/* /usr/share/aclocal/
+RUN yum install -y aclocal autoconf automake cmakegcc freetype-devel gcc gcc-c++ \
+	git lcms2-devel libjpeg-devel libjpeg-turbo-devel autogen autoconf libtool \
+	libpng-devel libtiff-devel libtool libwebp-devel libzip-devel make zlib-devel zip
 
 COPY build_tesseract.sh /tmp/build_tesseract.sh
 RUN chmod +x /tmp/build_tesseract.sh
-CMD sh /tmp/build_tesseract.sh
+ENTRYPOINT ["sh", "/tmp/build_tesseract.sh"]
--- build_tesseract.sh
+++ build_tesseract.sh
@@ -1,12 +1,19 @@
-#!/usr/bin/env bash
-TESSERACT_VERSION="4.1.1"
-
 # docker_build tesseract
+
 cd ~
-wget https://github.com/tesseract-ocr/tesseract/archive/$TESSERACT_VERSION.tar.gz
-ls
-tar -xzvf $TESSERACT_VERSION.tar.gz
-cd tesseract-$TESSERACT_VERSION
+git clone https://github.com/DanBloomberg/leptonica.git
+cd leptonica/
+git checkout $LEPTONICA_VERSION # newer version crashes tesseract build for now. See https://github.com/tesseract-ocr/tesseract/issues/3815
+./autogen.sh
+./configure
+make
+make install
+
+# tesseract
+cd ~
+git clone https://github.com/tesseract-ocr/tesseract.git
+cd tesseract
+git checkout $TESSERACT_VERSION
 export PKG_CONFIG_PATH=/usr/local/lib/pkgconfig
 ./autogen.sh
 ./configure
@@ -15,22 +22,38 @@ make install
 
 cd ~
 mkdir tesseract-standalone
+# copy files
 cd tesseract-standalone
-cp /usr/local/bin/tesseract .
+
+mkdir bin
+cp /usr/local/bin/tesseract ./bin
+
 mkdir lib
-cp /usr/local/lib/libtesseract.so.4 lib/
-cp /usr/local/lib/liblept.so.5 lib/
+cp /usr/local/lib/libtesseract.so.5 lib/
+cp /lib64/libpng15.so.15 lib/
+cp /lib64/libtiff.so.5 lib/
+cp /lib64/libgomp.so.1 lib/
+cp /lib64/libjbig.so.2.0 lib/
+cp /usr/local/lib/libleptonica.so.6 lib/
 cp /usr/lib64/libjpeg.so.62 lib/
 cp /usr/lib64/libwebp.so.4 lib/
-cp /usr/lib64/libpng15.so.15 lib/
+cp /usr/lib64/libstdc++.so.6 lib/
+
+# copy training data
 mkdir tessdata
 cd tessdata
 
-wget https://github.com/tesseract-ocr/tessdata_fast/raw/master/eng.traineddata
+wget https://github.com/tesseract-ocr/tessdata/blob/main/eng.traineddata
 
-mkdir configs
-cp /usr/local/share/tessdata/configs/pdf configs/
-cp /usr/local/share/tessdata/pdf.ttf .
+# archive
+cd ~
+cd tesseract-standalone
+mkdir python
 
-cd ..
-zip -r /tmp/build/tesseract.zip *
+cd ~
+# trim unneeded ~ 15 MB
+strip ./tesseract-standalone/**/*
+
+cd tesseract-standalone
+
+zip -r9 ../tesseract.zip *
